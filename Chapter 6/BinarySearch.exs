a59a161ff33558005be8c6b97b5eb7e49834984e
Aaron Lee <leeaaron326@gmail.com>
2019-01-15 23:47:15 -0500

    Removed IO.puts statement which causes Answer to not display because multiple threads are taking the output stream

--- Chapter 6/BinarySearch.exs
+++ Chapter 6/BinarySearch.exs
@@ -3,26 +3,22 @@ defmodule Chop do
   	"Answer: #{answer}"
   end
   def guess(guess, answer, first.._) when answer > guess do
-  	IO.puts "#{guess} #{first} - #{answer}"
   	guess(guess, first..answer)
   end
   def guess(guess, answer, _..last) when answer < guess do
-  	IO.puts "#{guess} #{answer} - #{last}"
   	guess(guess, answer..last)
   end
   def guess(guess, first..last) do
     middle = calcMiddle(first, last)
-   	IO.puts "Is it #{middle}? #{first} - #{last}"
    	guess(guess, middle, first..last)
   end
   def calcMiddle(first, last) do
     range = (last - first)
-    middle = first + div(range - 1, 2)
+    first + div(range - 1, 2)
   end
 end
 
 IO.puts Chop.guess(200, 1..1000)
 IO.puts Chop.guess(273, 1..1000)
-
 
 System.halt(0)
