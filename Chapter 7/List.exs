5783c67674a35a443eff2a24395df0ca46b4e53b
Lee <e087713@gh-4d9hyz1.corp.mastercard.org>
2019-01-21 17:44:47 -0500

    Added Span and Between Functions

--- Chapter 7/List.exs
+++ Chapter 7/List.exs
@@ -15,14 +15,33 @@ defmodule MyList do
   	my_max(tail, head)
   end
   def my_max([], running_max), do: running_max
-  def caesar([], n), do: []
-    def caesar([head|tail], n) when (head + n) > 122 do
+  def caesar([], _), do: []
+  def caesar([head|tail], n) when (head + n) > 122 do
   	[ rem(head + n, 122) | caesar(tail, n)]
   end
   def caesar([head|tail], n), do: [ head + n | caesar(tail, n)]
+  def span(from, to), do: span(from, to, [])
+  def span(from, to, ans) when from == to do
+    ans ++ [from]
+  end
+  def span(from, to, ans) do
+    span(from+1, to, ans ++ [from])
+  end
+  def between(from, to, list) do
+    between(from, to, list, [])
+  end
+  def between(0, 0, _, ans), do: ans
+  def between(from, to, [head|tail], ans) when from > 0 do
+    # Continue when from > 0 and to > 0
+    between(from-1, to-1, tail, ans)
+  end
+  def between(from, to, [head|tail], ans) when from == 0 do
+    # Append when from == 0 and to > 0
+    between(from, to-1, tail, ans ++ [head])
+  end
 end
 
-# Exercise: ListsAndRecursion - 1a
+# Exercise: ListsAndRecursion - 1
 IO.puts MyList.mapsum [1,2,3], &(&1 * &1)
 
 # Exercise: ListsAndRecursion - 2
@@ -33,5 +52,9 @@ IO.puts MyList.caesar('abcdef', 1)
 IO.puts MyList.caesar('abcdef', 122)
 IO.puts MyList.caesar('abcdef', 123)
 
-
 # Exercise: ListsAndRecursion - 4
+IO.puts MyList.span(97, 122)
+
+# Exercise: ListsAndRecursion - 5 (Mis-understood Question)
+testList = [97,98,99,100,101,102,103,104]
+IO.puts MyList.between(2, 5, testList)
