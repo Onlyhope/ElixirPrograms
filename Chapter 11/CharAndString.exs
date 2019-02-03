d8dea04a661fb6c5fa26856f67e1344da8cf5c4c
Aaron Lee <leeaaron326@gmail.com>
2019-02-02 23:11:03 -0500

    Added answer

--- Chapter 11/CharAndString.exs
+++ Chapter 11/CharAndString.exs
@@ -21,5 +21,18 @@ end
 IO.inspect MyCharacter.printableASCII('CAT')
 IO.inspect MyCharacter.printableASCII('j230`c\09dcj')
 
+# Exercise: StringsAndBinaries-2
 IO.inspect MyCharacter.anagram?('abc', 'cba')
 IO.inspect MyCharacter.anagram?('abc', 'bdb')
+
+# Exercise: StringsAndBinaries-3
+
+# Joined is actually a list with 'cat' as the head
+# Equivalent to [ ['c', 'a', 't'] | ['d', 'o', 'g']]
+full = 'catdog'
+joined = [ 'cat' | 'dog' ]
+
+IO.puts inspect full, charlists: :as_lists
+IO.puts inspect joined, charlists: :as_lists
+
+# Exercise: StringsAndBinaries-4
