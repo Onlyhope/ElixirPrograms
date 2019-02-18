f50737aea240f44f334e8f6a00e95f92994b45d7
malamute <leeaaron326@gmail.com>
2019-02-18 17:20:38 -0500

    Finished Exercise 7

--- Chapter 11/CalculateSalesTax.exs
+++ Chapter 11/CalculateSalesTax.exs
@@ -3,19 +3,39 @@
 defmodule CalculateSalesTax do
 	def readFile(file) do
 		case File.read(file) do
-			{:ok, content} -> parseContent(content)
+			{:ok, content} -> parseContent(String.split(content, "\n", trim: true));
 			{:error, reason} -> IO.puts "#{reason}"
 		end
 	end
 	def parseContent(content) do
-		IO.puts getLine(content)
+		content = removeHeaders(content)
+		Enum.map(content, &(process_data(&1)))
 	end
-	def getLine(""), do: IO.puts "End"
-	def getLine(<<line :: bitstring, "\n", rest :: bitstring>>) do
-		IO.puts line
+	def removeHeaders([head|tail]) do
+		IO.puts "Headers: " <> head |> String.split(",") |> Enum.map(&(&1 <> " "))
+		tail
+	end
+	def process_data(<<id :: bitstring-size(24), ",:", ship_to :: bitstring-size(16), ",", net_amount :: bitstring>>) do
+		{amount, _} = Float.parse(net_amount)
+		tax = salesTax(String.to_atom(ship_to))
+		amount * tax |> Float.round(2) |> reform(id, ship_to)
+	end
+	def reform(after_tax, id, ship_to) do
+		"#{id},#{ship_to},#{after_tax}"
+	end
+	def salesTax(state) do
+		taxMap = %{	
+			NC: 1.085,
+			OK: 1.075,
+			TX: 1.050,
+			MA: 1.060,
+			CA: 1.095
+		}
+		taxMap[state]
 	end
 end
 
-CalculateSalesTax.readFile("sales.csv");
+IO.inspect CalculateSalesTax.readFile("sales.csv");
+CalculateSalesTax.salesTax(String.to_atom("NC"))
 
 System.halt(0)
