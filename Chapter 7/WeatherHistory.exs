d96d5c8f22ec631e81d0984596ebe3ffaede5b47
e087713 <aaron.lee2@mastercard.com>
2019-02-21 14:00:23 -0500

    Updating weather example

--- Chapter 7/WeatherHistory.exs
+++ Chapter 7/WeatherHistory.exs
@@ -18,13 +18,25 @@ defmodule WeatherHistory do
 			[1366236422, 26, 17, 0.025]
 		]
 	end
-	def for_location([], _target_loc), do: []
-	def for_location([ [time, target_loc, temp, rain]|tail], target_loc) do
+	# Naive implementation of for_location(data, target_location)
+	def for_location_naive([], _target_loc), do: []
+	# This will match with the data points that has the target location
+	# and add the results of for_location(tail, target_loc) onto it
+	def for_location_naive([ [time, target_loc, temp, rain]|tail], target_loc) do
 		[ [time, target_loc, temp, rain] | for_location(tail, target_loc) ]
 	end
-	def for_location([_|tail], target_loc) do
+	# This will ignore all data points that doesn't match the target location
+	# and continue calling the for_location function with the tail
+	def for_location_naive([_|tail], target_loc) do
 		for_location(tail, target_loc)
 	end
+
+	# Sophisticated implementation of for_location(data, target_location)
+	def for_location([], _target_loc), do: []
+	def for_location([ head = [_, target_loc, _, _] | tail ], target_loc) do
+		[ head | for_location(tail, target_loc) ]
+	end
+	def for_location([_|tail], target_loc), do: for_location(tail, target_loc)
 end
 
 data = WeatherHistory.test_data()
