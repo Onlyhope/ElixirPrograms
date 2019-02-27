b05c0eeda7db5e1822b8a76de169746f4e3ca73d
e087713 <aaron.lee2@mastercard.com>
2019-02-27 13:31:31 -0500

    Adding ExceptionWrapper function

--- Chapter 12 - Control Flow/FizzBuzz.exs
+++ Chapter 12 - Control Flow/FizzBuzz.exs
@@ -23,7 +23,51 @@ defmodule FizzBuzz do
 	
 end
 
+defmodule ExceptionWrapper do
+	def ok!({status, result}) do
+		case {status, result} do
+			{:ok, _data} -> _data
+			{:error, _error} -> "Failed: #{_error}"
+		end
+	end
+end
+
 # ControlFlow-1: Rewrite the FizzBuzz example using case
 IO.inspect FizzBuzz.upto(16)
+
+# ControlFlow-2: 
+
+# I prefer the case implementation over the guard clause implementation.
+# Reasoning is all the cases are grouped up well in one function over the other
+# This is a matter of discrete states. Maybe in the case of continuous state
+# the guard clause method with when will be more appropriate
+# Condition is un-ideal because it's not necessarily a condition that must be met, 
+# but the output of two differenct condition will give you a certain case / state.
+
+# ControlFlow-3:
+
+file = ExceptionWrapper.ok! File.open("somefile.txt")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # System.halt(0)
