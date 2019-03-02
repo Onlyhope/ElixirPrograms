9a25393f6fda5c823d32bfdbddb43300cd079325
malamute <leeaaron326@gmail.com>
2019-03-02 11:54:01 -0500

    merging

--- Chapter 13 - Organizing a Project/issues/lib/issues/github_issues.ex
+++ Chapter 13 - Organizing a Project/issues/lib/issues/github_issues.ex
@@ -1,22 +1,23 @@
 defmodule Issues.GithubIssues do
-	@user_agent [ {"User-agent", "Elixir dave@pragprog.com"}]
+  @user_agent [ {"User-agent", "Elixir dave@pragprog.com"}]
+  @github_url Application.get_env(:issues, :github_url)
 
-	def fetch(user, project) do
-		issues_url(user, project)
-		|> HTTPoison.get(@user_agent)
-		|> handle_response
-	end
+  def fetch(user, project) do
+    issues_url(user, project)
+    |> HTTPoison.get(@user_agent)
+    |> handle_response
+  end
 
-	def issues_url(user, project) do
-		"https://api.github.com/repos/#{user}/#{project}/issues"
-	end
+  def issues_url(user, project) do
+    "https://api.github.com/repos/#{user}/#{project}/issues"
+  end
 
-	def handle_response({_, %{status_code: status_code, body: body}}) do
-		{
+  def handle_response({_, %{status_code: status_code, body: body}}) do
+    {
       status_code |> check_for_error(),
       body        |> Poison.Parser.parse!()
     }
-	end
+  end
 
   defp check_for_error(200), do: :ok
   defp check_for_error(_), do: :error
--- Chapter 13 - Organizing a Project/issues/mix.exs
+++ Chapter 13 - Organizing a Project/issues/mix.exs
@@ -5,7 +5,7 @@ defmodule Issues.MixProject do
     [
       app: :issues,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.0",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
