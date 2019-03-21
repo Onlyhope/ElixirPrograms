4fe3d029deb2d8c2057a307802cfc9e9e389db05
Aaron Lee <leeaaron326@gmail.com>
2019-03-20 23:29:41 -0400

    Finally solved Flatten

--- Chapter 10 - Processing Collections (Enum and Stream)/Flatten.exs
+++ Chapter 10 - Processing Collections (Enum and Stream)/Flatten.exs
@@ -1,18 +1,14 @@
 defmodule MyList do
+
 	def flatten(list), do: flatten(list, [])
 	def flatten([], answer), do: answer
+
 	def flatten([head|tail], answer) when is_list(head) do
-		IO.puts "#{head} is list"
-		flatten(head, tail, answer)
+		flatten(tail, answer ++ flatten(head))
 	end
+
 	def flatten([head|tail], answer) do
 		flatten(tail, answer ++ [head])
-	end
-	def flatten([], left_over, answer) do
-		flatten(left_over, answer)
-	end
-	def flatten([head|tail], left_over, answer) do
-		flatten(tail, left_over, answer ++ [head])
 	end
 
 end
@@ -24,13 +20,10 @@ IO.inspect testList1
 IO.puts "Output:"
 IO.inspect MyList.flatten(testList1)
 
-# Doesn't unroll the following case: [[[3]]]
+# To solve for the unrolling case=
 testList2 = [[[3]]]
 IO.puts "Input:"
 IO.inspect testList2
 
 IO.puts "Output:"
 IO.inspect MyList.flatten(testList2)
-
-
-# IO.inspect MyList.flatten(list)
