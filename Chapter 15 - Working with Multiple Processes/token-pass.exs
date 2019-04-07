06e0ceb450c9abe73e43d859b2e80b9f33342752
Aaron Lee <leeaaron326@gmail.com>
2019-04-07 10:16:51 -0400

    Adding Token-Pass

--- Chapter 15 - Working with Multiple Processes/token-pass.exs
+++ Chapter 15 - Working with Multiple Processes/token-pass.exs
@@ -1,6 +1,31 @@
+# Exercise: WorkingWithMultipleProcesses-2
 defmodule TokenPass do
 	
-	def pass do
-		
+	def receive_token do
+		receive do
+			{:token, token, sender} ->
+				IO.inspect sender
+				IO.puts "Hi, I've received your #{token}"
+				send(sender, {:message, "Send me another token please!", self()})
+				:timer.sleep(1000)
+			{:message, message, sender} ->
+				IO.inspect sender
+				IO.puts "Receiving message... #{message}"
+				IO.puts "Will do!"
+				send(sender, {:token, "ANOTHER_TOKEN", self()})
+				:timer.sleep(1000)
+		end
+		receive_token()
 	end
+
 end
+
+self()
+|> IO.inspect
+process_one = spawn(TokenPass, :receive_token, [])
+|> IO.inspect
+process_two = spawn(TokenPass, :receive_token, [])
+|> IO.inspect
+
+send(process_one, {:token, "A_TOKEN", process_two})
+
