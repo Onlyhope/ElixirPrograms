458140d5607ad09ecde8f75ce27c2f59ce00903c
Aaron Lee <leeaaron326@gmail.com>
2019-04-14 18:32:24 -0400

    Kinda got it to work

--- Chapter 16 - Nodes (The Keys to Distributing Services)/ticker-exercise-3.exs
+++ Chapter 16 - Nodes (The Keys to Distributing Services)/ticker-exercise-3.exs
@@ -2,13 +2,15 @@ defmodule Ticker do
 	
 	@interval 2000 #2 seconds
 	@name :ticker 
+	@first :first_client
+	@last :last_client
 
 	@doc """
 		Spawns a server process that registers
 		the PID of this server under the name :ticker.
 	"""
 	def start do
-		pid = spawn(__MODULE__, :generator, [])
+		pid = spawn(__MODULE__, :generator, [[]])
 		:global.register_name(@name, pid)
 	end
 
@@ -19,23 +21,39 @@ defmodule Ticker do
 		send(:global.whereis_name(@name), {:register, client_pid})
 	end
 
+	def generator([]) do
+		receive do
+			{:register, pid} ->
+				IO.puts "Registering #{inspect(pid)} as first"
+				:global.register_name(@first, pid)
+				generator([pid])
+		end
+	end
+
 	@doc """
 		Sends out a tick to all its client every 2 seconds.
 		Also register clients to the list of processes
 	"""
-	def generator() do
+	def generator(clients) do
 		receive do
 			{:register, pid} ->
-				IO.puts "Registering #{inspect(pid)}"
-				generator()
-			after
-				@interval ->
-					# current_time = :os.system_time(:millisecond)
-					IO.puts "tick"
-
-					# Re-cursively call generator to tick again
-					generator(tail, processed_clients ++ [head])	
+				IO.puts "Registering #{inspect(pid)} as last"
+				:global.register_name(@last, pid)
+				# Update the client list that's currently being ran
+				generator(clients ++ [pid])
+			{:tick} ->
+				[head|tail] = clients
+				IO.puts "Clients: #{inspect clients}"
+				IO.puts "tick -> #{inspect head}"
+				send(head, {:tick, tail, []})
+				generator(clients)
 		end
+	end
+
+	def tick() do
+		IO.puts "tick"
+		:timer.sleep(2000)
+		send(:global.whereis_name(@name), {:tick})
 	end
 
 end
@@ -46,32 +64,23 @@ defmodule Client do
 		Spawns a process and register it to Ticker
 	"""
 	def start do
-		pid = spawn(__MODULE__, :receiver, [[], []])
+		pid = spawn(__MODULE__, :receiver, [])
 		Ticker.register(pid)
 	end
 
 	def receiver do
 		receive do
-			{ :tick, {[], []} } ->
+			{:tick, [], []} ->
 				receiver() 
-			{ :tick, {[head|tail], processed_clients} } ->
-				IO.puts "tick"
-			after
-				@interval -> 
-					IO.puts "#{inspect self()} tock in client"
-
-					send(head, {:tick, {tail, processed_clients ++ [head]}}) 
-
-					receiver()
-			{ :tick, {[], [head|tail]} } ->
-				IO.puts "tick"
-			after
-				@interval ->
-					IO.puts "#{inspect self()} tock in client"
-
-					send(head, {:tick, {tail, [head]}})
-
-					receiver()
+			{:tick, [head|tail], processed_clients} ->
+				IO.puts "#{inspect self()} tock in client"
+				:timer.sleep(2000)
+				IO.puts "#{inspect self()} tick -> #{inspect head}"
+				send(head, {:tick, tail, processed_clients ++ [head]})
+				receiver()
+			{:tick, [], _processed_clients} ->
+				IO.puts "#{inspect self()} tock in client"
+				receiver()
 		end
 	end
 
@@ -82,6 +91,10 @@ end
 # Ticker
 
 Ticker.start
+Client.start
+Client.start
+Client.start
+Ticker.tick
 
 # Each Client should have a listener for :tick
 
