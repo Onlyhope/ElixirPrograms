c4f06fa4a1e149e09d644b15b6d0ed5bdce4e1f0
Aaron Lee <leeaaron326@gmail.com>
2019-04-20 11:15:56 -0400

    Finalizing implementation of stack-server

--- Chapter 17 - OTP Servers/stack-server.ex
+++ Chapter 17 - OTP Servers/stack-server.ex
@@ -2,12 +2,35 @@ defmodule StackServer do
 	
 	use GenServer
 
+	@server __MODULE__
+
+	def serverName do
+		@server
+	end
+
+	def start do
+		GenServer.start_link(StackServer, :ok, name: @server)
+		GenServer.call(@server, :pop)
+	end
+
+	def pop do
+		GenServer.call(@server, :pop)
+	end
+
+	def push(content) do
+		GenServer.cast(@server, {:push, content})
+	end
+
+	def stop(reason) do
+		GenServer.cast(@server, {:stop, reason})
+	end
+
 	def init(initial_content) do 
 		{:ok, [initial_content]}
 	end
 
 	def handle_call(:pop, _from, []) do
-		{:reply, [], []}  #{:reply, result, new_state}
+		{:reply, nil, []}  #{:reply, result, new_state}
 	end
 
 	def handle_call(:pop, _from, [head|tail]) do
@@ -19,11 +42,22 @@ defmodule StackServer do
 		# Can also return {:stop, reason, new_state}
 	end
 
+	def handle_cast({:stop, reason}, current_state) do
+		{:stop, reason, current_state}
+	end
+
+	def terminate(reason, state) do
+		IO.puts "Terminating... #{inspect reason}"
+		IO.puts "State: #{inspect state}"
+	end
+
 	# handle_call(request, from, state)
 	# handle_cast(request, state)
 	# handle_info(info, state)
 	# terminate(reason, state)
 	# code_change(from_version, state, extra)
 	# format_status(reason, [pdict, state])
+
+
 
 end
