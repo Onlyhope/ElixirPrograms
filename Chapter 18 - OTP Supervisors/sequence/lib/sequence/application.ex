c16f7c8028b51548f93ede39974d1e99ee3e26b8
Lee, Aaron <aaron.lee2@mastercard.com>
2019-04-22 16:44:56 -0400

    Adding supervisor code

--- Chapter 18 - OTP Supervisors/sequence/lib/sequence/application.ex
+++ Chapter 18 - OTP Supervisors/sequence/lib/sequence/application.ex
@@ -10,6 +10,7 @@ defmodule Sequence.Application do
     children = [
       # Starts a worker by calling: Sequence.Worker.start_link(arg)
       # {Sequence.Worker, arg}
+      {Sequence.Server, 100}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
