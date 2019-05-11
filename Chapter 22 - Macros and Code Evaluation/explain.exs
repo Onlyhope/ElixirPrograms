955c3ea8f6d923716785717011d600c7beb57a70
Aaron Lee <leeaaron326@gmail.com>
2019-05-11 18:48:16 -0400

    Adding work for explain

--- Chapter 22 - Macros and Code Evaluation/explain.exs
+++ Chapter 22 - Macros and Code Evaluation/explain.exs
@@ -1,7 +1,21 @@
-defmodule Explain do
+defmodule Macro do
 	
+	defmacro explain(arg) do
+		IO.inspect(arg)
+	end
+
 end
 
 defmodule Test do
-	
+
+	require Macro
+
+	Macro.explain(do: 2 + 3 * 4)
+	[
+		do: {:+, 
+				[line: 13],
+				[2, {:*, [line: 13], [3, 4]}
+				]
+		}
+	]
 end
