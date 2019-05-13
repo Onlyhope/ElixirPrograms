0f45f2ef12c556287196ff2605145a3b0fcb38f2
Aaron Lee <leeaaron326@gmail.com>
2019-05-12 23:08:35 -0400

    Adding notes on macros and tracer code

--- Chapter 22 - Macros and Code Evaluation/my-unless.exs
+++ Chapter 22 - Macros and Code Evaluation/my-unless.exs
@@ -29,4 +29,49 @@ defmodule Test do
 
 end
 
+# quote do => Turns code into code fragment
+# quote do: 1 + 1, will result in a tuple
+# {:+, [_line_number], [1,1]}
 
+# when you see quote: it says "interpret the content of the block
+# that follows as code, and return the internal representation"
+# - Dave Thomas
+
+# unquote can only be use within quote blocks
+# unquote can be called inject_code_fragment
+# so if you have code_fragment to be used within
+# a quote block you must use unquote to incorporate
+# it into the quote block. Otherwise Elixir will
+# read it is normal code.
+
+# Example:
+
+# quote do
+# 	if !condition do
+# 		do_clause
+# 	end
+# end
+
+# Will be read as:
+
+# quote do
+# 	if !condition() do
+# 		do_clause()
+# 	end
+# end
+
+# While
+
+# quote do
+# 	if !unquote(condition) do
+# 		unquote(do_clause)
+# 	end
+# end
+
+# Will be read as:
+
+# quote do
+# 	if !CONDITION_FRAGMENT do
+# 		DO_CLAUSE_FRAGMENT
+# 	end
+# end
--- Chapter 23 - Linking Modules (Behaviours and Use)/tracer.ex
+++ Chapter 23 - Linking Modules (Behaviours and Use)/tracer.ex
@@ -1,8 +1,57 @@
+defmodule Tracer do
+
+	def dump_defn(name, args) do
+		"{name}(#{dump_args(args)})"
+	end
+
+	def dump_args(args) do
+		args |> Enum.map(&inspect/1) |> Enum.join(", ")
+	end
+
+	defmacro def(definition = {name, _, args}, do: content) do
+		quote do
+			Kernel.def(unquote(definition)) do
+				IO.puts "==> call: #{Tracer.dump_defn(unquote(name), unquote(args))}"
+				result = unquote(content)
+				IO.puts "<== result: #{inspect result}"
+				result
+			end
+		end
+	end
+
+	defmacro __using__(_opts) do
+		quote do
+			import Kernel, except: [def: 2]
+			import unquote(__MODULE__), only: [def: 2]
+		end
+	end
+end
+
 defmodule Test do
 	use Tracer
-	def puts_sum_three(a,b,c) do: IO.inspect(a + b + c)
+	def puts_sum_three(a,b,c), do: a + b + c
 	def add_list(list), do: Enum.reduce(list, 0, &(&1 + &2))
 end
 
 Test.puts_sum_three(1,2,3)
 Test.add_list([5,6,7,8])
+
+# Exercise: LinkingModulesBeavioursAndUse-1
+# In the body of the def macro, there's a quote block that defines
+# the actual method. Why does the first call to puts have to unquote
+# the values in its interpolation but the second call does not?
+
+# The result is locally scoped and is already a value. While, name
+# and args is scope to the macro. It is still a code fragment and must 
+# be unquoted, otherwise Elixir will evaluated as function calls or variables
+
+# Exercise: LinkingModules-BehavioursAndUse-2
+
+# The built-in module IO.ANSI defines functions that represent ANSI
+# escape sequences. You can use it it to build output than will display
+# (for example) colors and bold, inverse, or underlined text (assuming 
+# the terminal supports it)
+# Explore the module and use it to colorize our tracing's output
+# Why does passing a list of strings to IO.puts work?
+
+
